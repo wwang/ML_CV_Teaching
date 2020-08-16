87d55607d46cd69efff8219cfaf7241182d6ec41
wwang <wwang.work@gmail.com>
2020-08-16 16:09:35 -0500

    add file for handwrite digits

---
+++ handwrite_digits/handwrite_digits_pkg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# just a little function for pretty printing a matrix
+def matprint(mat, fmt="g"):
+    col_maxes = [max([len(("{:"+fmt+"}").format(x)) for x in col]) for col in mat.T]
+    for x in mat:
+        for i, y in enumerate(x):
+            print(("{:"+str(col_maxes[i])+fmt+"}").format(y), end="  ")
+        print("")
