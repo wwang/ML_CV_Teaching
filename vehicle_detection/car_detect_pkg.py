0a3c9161a3d8805ce03933d442a19ff648562267
wwang <wwang.work@gmail.com>
2020-08-20 15:28:04 -0500

    simplifying vehicle detection

--- vehicle_detection/car_detect_pkg.py
+++ vehicle_detection/car_detect_pkg.py
@@ -829,7 +829,7 @@ def process_video_clip1(clip, model, scaler):
 
     return output_video
 
-def load_video_clip3():
+def load_video_clip2():
     # long video
     clip2_path = "./ExploreSTEM/vehicle_detection/project_video.mp4"
     clip = VideoFileClip("./ExploreSTEM/vehicle_detection/project_video.mp4")
@@ -837,7 +837,7 @@ def load_video_clip3():
 
     return clip, clip2_path
     
-def process_video_clip3(clip, model, scaler):
+def process_video_clip2(clip, model, scaler):
     video_processor = VideoProcessor(model, scaler)
     output_video = "./project_video_out.mp4"
     output_clip = clip.fl_image(lambda image: process_image(image,video_processor))
@@ -848,7 +848,7 @@ def process_video_clip3(clip, model, scaler):
     return (output_video)
 
 
-def load_video_clip2():
+def load_video_clip3():
     # long video
     clip2_path = "./ExploreSTEM/vehicle_detection/Shorter Video.mp4"
     clip = VideoFileClip(clip2_path)
@@ -856,7 +856,7 @@ def load_video_clip2():
 
     return clip, clip2_path
     
-def process_video_clip2(clip, model, scaler):
+def process_video_clip3(clip, model, scaler):
     video_processor = VideoProcessor(model, scaler)
     output_video = "./shorter_video_out.mp4"
     output_clip = clip.fl_image(lambda image: process_image(image,video_processor))
