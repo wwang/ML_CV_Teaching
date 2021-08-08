e5ae40c4b3964146004cfbfe1fee3e719add9788
Wei Wang <wwang.work@gmail.com>
2021-08-08 17:56:02 -0500

    add hsv function

--- color_segmentation/color_segmentation.py
+++ color_segmentation/color_segmentation.py
@@ -8,7 +8,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-def show_rgb_distribution(img):
+def show_rgb_dist(img):
 
     # extract the red, green and blue pixels from the picture
     r, g, b = cv2.split(img)
@@ -29,4 +29,22 @@ def show_rgb_distribution(img):
     axis.set_xlabel("Red")
     axis.set_ylabel("Green")
     axis.set_zlabel("Blue")
+    plt.show()
+
+def show_hsv_dist(img):
+    # convert RGB to HSV
+    hsv_nemo = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+    # extract the hue, saturation, and value
+    h, s, v = cv2.split(hsv_nemo)
+
+    fig = plt.figure()
+    fig.set_figheight(7) # 1.18in or 3cm
+    fig.set_figwidth(12) # 2.75in or 7cm
+    axis = fig.add_subplot(1, 1, 1, projection="3d")
+
+    axis.scatter(h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker=".")
+    axis.set_xlabel("Hue")
+    axis.set_ylabel("Saturation")
+    axis.set_zlabel("Value")
     plt.show()
